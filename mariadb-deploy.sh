ac0e5affc017662ecfeb5ae35fa29e3f379437fe
swashy-stable <roland.macdavid@gmail.com>
2018-07-10 16:43:49 -0400

    some messy checks so i can run this script alot

--- mariadb-deploy.sh
+++ mariadb-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 touch /etc/yum.repos.d/MariaDB.repo
 
+if [ ! -s /etc/yum.repos.d/MariaDB.repo ]
+then
 cat >> /etc/yum.repos.d/MariaDB.repo <<__FILE__
 # MariaDB 10.3 CentOS repository list - created 2018-07-10 19:39 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
@@ -10,11 +12,12 @@ baseurl = http://yum.mariadb.org/10.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 __FILE__
+fi
 
 yum install epel-release -y
 yum install MariaDB-server MariaDB-client ntp -y
 systemctl enable ntpd
-systecmtl start ntpd
+systemctl start ntpd
 
 sed '$d' /etc/security/limits.conf > /etc/security/limits.conf
 echo -e "*                -       nofile          16384" >> /etc/security.limits.conf
@@ -28,10 +31,11 @@ cat >> /etc/systemd/journald.conf <<__FILE__
 Storage=persistent
 SystemMaxUse=500M
 __FILE__
-mkdir /var/log/journal
 systemd-tmpfiles --create --prefix /var/log/journal/
 systemctl restart systemd-journald;
 
+if [ ! -s /etc/profile.d/color-prompt.sh ]
+then
 cat >> /etc/profile.d/color-prompt.sh <<__FILE__
 # vim:ts=4:sw=4
 if [[ $EUID -ne 0 ]]; then
@@ -40,12 +44,15 @@ else
     PS1='[\[\033[91m\]\u\[\033[m\]@\[\033[01;32m\]\H\[\033[00m\] \W]\$ '
 fi
 __FILE__
+fi
 
 cat >> /etc/profile.d/instant-history.sh <<__FILE__
 shopt -s histappend
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 __FILE__
 
+if [ ! -s /etc/firewalld/zones/private.xml ]
+then
 cat >> /etc/firewalld/zones/private.xml <<__FILE__
 <?xml version="1.0" encoding="utf-8"?>
 <zone>
@@ -55,6 +62,7 @@ cat >> /etc/firewalld/zones/private.xml <<__FILE__
   <service name="mysql"/>
 </zone>
 __FILE__
+fi
 
 systemctl enable firewalld
-systemctl enable firewalld
+systemctl start firewalld
