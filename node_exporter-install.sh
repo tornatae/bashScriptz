fd32e864aaf80dbd01f66ef3fc461902a248fb47
rmacdavid <rmacdavid@linode.com>
2018-12-26 15:25:20 -0500

    enhance with variables

--- node_exporter-install.sh
+++ node_exporter-install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#!/bin/bash
+GIT_URL=https://github.com/prometheus/node_exporter/releases/download/v0.17.0/node_exporter-0.17.0.linux-amd64.tar.gz
+TAR=node_exporter-0.17.0.linux-amd64.tar.gz
+DIR=node_exporter-0.17.0.linux-amd64
+
+PROM_IP=0.0.0.0
+
 # If available, use LSB to identify distribution
 if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
     export DISTRO=$(lsb_release -i 2>/dev/null | cut -d: -f2 | sed s/'^\t'//)
@@ -18,16 +23,12 @@ main () {
        return 1
     fi
     cd  ;
-    #wget https://github.com/prometheus/node_exporter/releases/download/v0.15.1/node_exporter-0.15.1.linux-amd64.tar.gz
-    wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz
-    #tar -xvf node_exporter-0.15.1.linux-amd64.tar.gz
-    tar -xvf node_exporter-0.16.0.linux-amd64.tar.gz
-    #mv node_exporter-0.15.1.linux-amd64 node_exporter
-    mv node_exporter-0.16.0.linux-amd64 node_exporter
+    wget $GIT_URL 
+    tar -xvf $TAR
     useradd -s /bin/false/ node_exporter
-    chown -R node_exporter: node_exporter/
+    chown -R node_exporter: $DIR 
 
-    cp node_exporter/node_exporter /usr/bin/node_exporter
+    cp $DIR/node_exporter /usr/bin/node_exporter
 
     touch /etc/systemd/system/node_exporter.service
     cat > /etc/systemd/system/node_exporter.service<<__FILE__
@@ -44,27 +45,30 @@ RestartSec=5
 WantedBy=default.target
 __FILE__
 
-firewalld_running=$(systemctl list-units --type=service --state=running | grep firewalld)
-ufw_running=$(systemctl list-units --type=service --state=active --no-pager | grep ufw)
+FIREWALLD_RUNNING=$(systemctl list-units --type=service --state=running | grep firewalld)
+FIREWALLD_ENABLED=$(systemctl list-units --type=service --state=enabled | grep firewalld)
 
-if [[ "$DISTRO" == 'centos' && -n "$firewalld_running" ]]
+UFW_RUNNING=$(systemctl list-units --type=service --state=running --no-pager | grep ufw)
+UFW_ENABLED=$(systemctl list-units --type=service --state=enabled --no-pager | grep ufw)
+
+IPTABLES_ENABLED=$(systemctl list-unit-files --state=enabled --no-pager | grep netfilter)
+
+if [[ -n "$FIREWALLD_ENABLED" ]]
 then
 firewall-cmd --permanent --zone=public --add-rich-rule='
   rule family="ipv4"
-  source address="97.107.134.73/32"
-  port protocol="tcp" port="9104" accept'
-firewall-cmd --permanent --zone=public --add-rich-rule='
-  rule family="ipv4"
-  source address="97.107.134.73/32"
+  source address="$PROM_IP"
   port protocol="tcp" port="9100" accept'
-fi
 
-if [[ "$DISTRO" == "Ubuntu" && -n "$ufw_running" ]]
+elif [[ -n "$UFW_ENABLED" ]]
 then
-    ufw allow from 97.107.134.73/32 to any port 9100
-    ufw allow from 97.107.134.73/32 to any port 9104
-fi
+    ufw allow from $PROM_IP to any port 9100
 
+elif [[ -n "$IPTABLES_ENABLED" ]]
+then
+    iptables -I INPUT -p tcp -s $PROM_IP --dport 9100 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 9100 -j DROP
+fi
 
 systemctl enable node_exporter
 systemctl start node_exporter
